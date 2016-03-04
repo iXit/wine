6f5ada90ea8b3ae3c4dea092d598c9f2d8508266
Axel Davy <axel.davy@ens.fr>
2016-03-04 21:38:50 +0100

    d3d9-nine: Add device and swapchain wrappers
    This enables the Steam overlay to work
    
    Signed-off-by: Axel Davy <axel.davy@ens.fr>

--- dlls/d3d9-nine/d3dadapter9.c
+++ dlls/d3d9-nine/d3dadapter9.c
@@ -30,6 +30,7 @@ WINE_DEFAULT_DEBUG_CHANNEL(d3d9nine);
 
 #include <d3dadapter/d3dadapter9.h>
 #include "present.h"
+#include "device_wrap.h"
 
 /* This represents a snapshot taken at the moment of creation */
 struct output
@@ -468,6 +469,14 @@ static HRESULT WINAPI DECLSPEC_HOTPATCH d3dadapter9_CreateDeviceEx(struct d3dada
         return hr;
     }
 
+    /* Nine returns different vtables for Ex, non Ex and
+     * if you use the multithread flag or not. This prevents
+     * things like Steam overlay to work, in addition to the problem
+     * that functions nine side are not recognized by wine as
+     * hotpatch-able. If possible, we use our vtable wrapper,
+     * which solves the problem described above. */
+    if (enable_device_vtable_wrapper())
+        (*ppReturnedDeviceInterface)->lpVtbl = get_device_vtable();
     return hr;
 }
 
